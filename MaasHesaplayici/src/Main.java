ede1e14419df4092f699ef9f9a7234279f9e9958
yagmurigde <igdeyagmur@gmail.com>
2023-10-07 22:29:06 +0300

    commit

---
+++ MaasHesaplayici/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Kerem Budha", 50000, 15, 2020);
+        System.out.println(employee.toString());
+
+    }
+}
